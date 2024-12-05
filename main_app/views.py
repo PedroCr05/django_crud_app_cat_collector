4740f2f80f13a9103d502fbd99de788471c72db4
pedrocruz <JayJMarquez05@gmail.com>
2024-12-05 12:05:11 -0600

    Created some mock data for the route.

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.shortcuts import render
 
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+cats = [
+    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
 # Import HttpResponse to send text-based responses
 from django.http import HttpResponse
 
@@ -11,3 +25,6 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def cat_index(request):
+    return render(request, 'cats/index.html', { 'cats': cats })
